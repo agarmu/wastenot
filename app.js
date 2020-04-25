0bd8c7d35d977cf38fa31b6b5b3305b661cf6cc3
Mukul <55563106+agarmu@users.noreply.github.com>
2020-04-25 10:31:57 -0500

    standardization

--- app.js
+++ app.js
@@ -15,9 +15,8 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);

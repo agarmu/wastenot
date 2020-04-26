77e22f57da38f6cd943f4f1814cb329bae027084
Mukul <55563106+agarmu@users.noreply.github.com>
2020-04-26 01:31:29 -0500

    Deployment Checking

--- Model/index.js
+++ Model/index.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
+//require('dotenv').config();
 
 
-mongoose.connect(process.env.MONGOS, { useNewUrlParser: true, useUnifiedTopology: true }, (error)=>{
+mongoose.connect("MONGOS = mongodb+srv://user-one:3P4HA3AbKSECzo7b@cluster0-9pg8u.azure.mongodb.net/App?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }, (error)=>{
   if(error)
     {
       console.log("Error connecting to database");

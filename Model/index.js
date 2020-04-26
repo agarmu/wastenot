f7fbd340b660c44cd532849fc4025fcd71a9ee68
Mukul <55563106+agarmu@users.noreply.github.com>
2020-04-26 12:31:46 -0500

    Data Display with modal

--- Model/index.js
+++ Model/index.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-//require('dotenv').config();
+require('dotenv').config();
 
 
-mongoose.connect("mongodb+srv://user-one:3P4HA3AbKSECzo7b@cluster0-9pg8u.azure.mongodb.net/App?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }, (error)=>{
+mongoose.connect(process.env.MONGOS, { useNewUrlParser: true, useUnifiedTopology: true }, (error)=>{
   if(error)
     {
       console.log("Error connecting to database");

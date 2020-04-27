f166a700f86e449b4dfa1a2c42a25014b2a6a6e4
Mukul <55563106+agarmu@users.noreply.github.com>
2020-04-27 12:33:49 -0500

    Fixed double adding of entries

--- routes/donate.js
+++ routes/donate.js
@@ -6,6 +6,9 @@ const DonationsModel = mongoose.model("donations");
 const request = require('request');
 let formData = {}
 /* GET home page. */
+function isEmpty(obj) {
+  return Object.keys(obj) == 0;
+}
 router.get('/', function(req, res, next) {
   res.render('donate', { 
     title: 'Donate',
@@ -15,42 +18,49 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.post('/',[
+router.post('/',
+  [
 
-  check('name')
-    .isLength({min : 1})
-    .withMessage('Name Empty'),
-  check('email')
-    .isEmail()
-    .withMessage("Email Invalid"),
-  check('phone')
-    .isNumeric()
-    .withMessage("Phone Number Invalid"),
-  check('address')
-    .trim()
-    .isLength({min: 1})
-    .withMessage("Address Empty"),
-  check('cuisine')
-    .isAlpha()
-    .withMessage("Invalid Cuisine Type"),
-  check('amt')
-    .isNumeric()
-    .withMessage("Quantity must be a number"),
+    check('name')
+      .isLength({min : 1})
+      .withMessage('Name Empty'),
+    check('email')
+      .isEmail()
+      .withMessage("Email Invalid"),
+    check('phone')
+      .isNumeric()
+      .withMessage("Phone Number Invalid"),
+    check('address')
+      .trim()
+      .isLength({min: 1})
+      .withMessage("Address Empty"),
+    check('cuisine')
+      .isAlpha()
+      .withMessage("Invalid Cuisine Type"),
+    check('amt')
+      .isNumeric()
+      .withMessage("Quantity must be a number"),
 
-], (req, res, next)=>{
+  ], 
+  (req, res, next)=>{
 
   const errors = validationResult(req);
   
-  if(errors.isEmpty){
+  if(isEmpty(errors)){
     const timestamp = new Date();
     const name = req.body.name;
     const email = req.body.email;
     const phone = parseInt(req.body.phone);
     const addr = req.body.address;
-    const geodata = request(`http://photon.komoot.de/api/?q=${addr.replace(/\s/g,'+')}`, { json: true }, (err, resp, body) => {
-      if(err) { return console.log(err); }
-       return body;
-    });
+    const geodata = 
+      request(
+        `http://photon.komoot.de/api/?q=${addr.replace(/\s/g,'+')}`, 
+        { json: true }, 
+        (err, resp, body) => {
+          if(err) { return {}; }
+          return body;
+        }
+      );
     const orgtype = req.body.orgtype;
     const cuisine = req.body.cuisine;
     const foodName = req.body.food;
@@ -58,8 +68,8 @@ router.post('/',[
     const foodUnits = req.body.units;
     const isHalal = (req.body.halal == 'yes') || false;
     const isKosher = (req.body.kosher == 'yes') || false;
-    const isVegetarian = (req.body.vegetarian == 'yes') || false;
-    const isVegan = ((req.body.vegan == 'yes') && isVegetarian) || false;
+    const isVegan = (req.body.vegan == 'yes') || false;
+    const isVegetarian = ((req.body.vegetarian == 'yes') ||isVegan) || false;
     let data = {
       timestamp: timestamp,
       name: name,
